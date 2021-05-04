170a0554ff9556b7e9e24de3572d653bbdcfacdc
sinbox0701 <psh090953@gmail.com>
2021-05-04 09:36:46 +0900

    Moving Through Screens

--- screens/Login.js
+++ screens/Login.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { View,Text } from "react-native";
+import { View,Text,TouchableOpacity } from "react-native";
 
-export default function Login(){
+export default function Login({navigation}){
     return (
         <View>
             <Text>Login</Text>
+            <TouchableOpacity onPress={()=>navigation.navigate("CreateAccount")}>
+                <View>
+                    <Text>Go to CreateAccount</Text>
+                </View>
+            </TouchableOpacity>
         </View>
+        
     );
 }
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { View,Text } from "react-native";
+import { View,Text,TouchableOpacity } from "react-native";
 
-export default function Welcome(){
+export default function Welcome({navigation}){
     return (
         <View>
             <Text>Welcome</Text>
+            <TouchableOpacity onPress={()=>navigation.navigate("CreateAccount")}>
+                <View>
+                    <Text>Go to CreateAccount</Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>navigation.navigate("Login")}>
+                <View>
+                    <Text>Go to Login</Text>
+                </View>
+            </TouchableOpacity>
         </View>
     );
 }
