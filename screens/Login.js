34bd9b1f283e9c214806e3097ede94a7cf926504
sinbox0701 <psh090953@gmail.com>
2021-05-04 15:52:50 +0900

    React Hook Form

--- screens/Login.js
+++ screens/Login.js
@@ -1,23 +1,44 @@
-import React from "react";
-import { View,Text,TouchableOpacity } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { useForm } from "react-hook-form";
+import AuthButton from "../components/auth/AuthButton";
 import AuthLayout from "../components/auth/AuthLayout";
 import { TextInput } from "../components/auth/AuthShared";
 
-export default function Login({navigation}){
+export default function Login(){
+    const {register,handleSubmit,setValue} = useForm();
+    const passwordRef = useRef();
+    const onNext = (nextOne) => {
+        nextOne?.current?.focus();
+    };
+    const onValid = (data) => {
+        console.log(data);
+    };
+    useEffect(()=>{
+        register("username");
+        register("password");
+    },[register]);
+
     return (
         <AuthLayout>
             <TextInput
                 placeholder="Username"
+                autoCapitalize="none"
                 returnKeyType="next"
                 placeholderTextColor={"rgba(255,255,255,0.6)"}
+                onSubmitEditing={()=>onNext(passwordRef)}
+                onChangeText={(text)=>setValue("username",text)}
             />
             <TextInput
+                ref={passwordRef}
                 placeholder="Password"
                 secureTextEntry
                 returnKeyType="done"
                 lastOne={true}
                 placeholderTextColor={"rgba(255,255,255,0.6)"}
+                onSubmitEditing={handleSubmit(onValid)}
+                onChangeText={(text)=>setValue("password",text)}
             />
+            <AuthButton text="Login" disabled={true} onPress={handleSubmit(onValid)} />
         </AuthLayout>
         
     );
