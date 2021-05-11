1a62be7518316a3a6ffcf216c9cf108a7ed9b767
sinbox0701 <psh090953@gmail.com>
2021-05-11 10:19:12 +0900

    Bottom Tabs Navigator

--- navigators/LoggedInNav.js
+++ navigators/LoggedInNav.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import TabsNav from "./TabsNav";
-import Upload from "../screens/Upload";
+import UploadNav from "./UploadNav";
 
 const Stack = createStackNavigator();
 
@@ -9,13 +9,7 @@ export default function LoggedInNav() {
     return (
         <Stack.Navigator headerMode="none" mode="modal">
             <Stack.Screen name="Tabs" component={TabsNav} />
-            <Stack.Screen name="Upload" component={Upload}/>
+            <Stack.Screen name="Upload" component={UploadNav}/>
         </Stack.Navigator>
     );
 }
-
-// Navigator
-/*
- Stack --> Tab --> Stack 구조
- Tab위에 Stack을 만드는 이유는 사진을 업로드할때 현재 만들어논 Tab이 아닌 전혀 다른 페이지로 이동해야하기 때문
-*/
